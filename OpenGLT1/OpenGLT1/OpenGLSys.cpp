b2d862edfa00faed4d6c379e896a4693f86bccfa
Peter Lockett <0901632@live.abertay.ac.uk>
2012-08-21 13:17:03 +0100

    Using GLFW
    Using GLFW for multi-platform window creation and input handling,
    removes a lot of really boring code to look at, extremely simple to set
    up.

--- OpenGLT1/OpenGLT1/OpenGLSys.cpp
+++ OpenGLT1/OpenGLT1/OpenGLSys.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <Windows.h>
-#include <gl/GL.h>
-#include <gl/GLU.h>
+#include "glfw.h"
 #include "OpenGLSys.h"
 
 OpenGLSys::OpenGLSys(){}
@@ -19,7 +16,6 @@ bool OpenGLSys::Init()
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
 	m_rotation = 0.0f;
-
 	return true;
 }
 
@@ -79,64 +75,60 @@ void OpenGLSys::Update(float dt)
 
 void OpenGLSys::Render()
 {
-
-	/* These are to calculate our fps */
-	static GLint T0     = 0;
-	static GLint Frames = 0;
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen AND depth, it's a flag (fancy eh?)
 	glLoadIdentity(); // Reset modelview matrix
 
-	glTranslatef(0.0f, 0.0f, -7.0f);
-	glRotatef(m_rotation, 1.0f, 0.5f, 0.0f); // Rotate the cube on the X and Y axis
+	glPushMatrix();
+		glTranslatef(0.0f, 0.0f, -7.0f);
+		glRotatef(m_rotation, 1.0f, 0.5f, 0.0f); // Rotate the cube on the X and Y axis
+		glBegin(GL_QUADS);
+			// TOP
+			glColor3f(0.0f, 1.0f, 0.0f); // Green
+			glVertex3f( 1.0f, 1.0f,-1.0f); // Top Right
+			glVertex3f(-1.0f, 1.0f,-1.0f); // Top Left
+			glVertex3f(-1.0f, 1.0f, 1.0f); // Bottom Left
+			glVertex3f( 1.0f, 1.0f, 1.0f); // Bottom Right
+			// Bottom
+			glColor3f(1.0f, 0.5f, 0.0f); // Orange
+			glVertex3f( 1.0f,-1.0f, 1.0f);
+			glVertex3f(-1.0f,-1.0f, 1.0f);
+			glVertex3f(-1.0f,-1.0f,-1.0f);
+			glVertex3f( 1.0f,-1.0f,-1.0f);
+			// Front
+			glColor3f(1.0f, 0.0f, 0.0f); // Red
+			glVertex3f( 1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f,-1.0f, 1.0f);
+			glVertex3f( 1.0f,-1.0f, 1.0f);
+			// Back
+			glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+			glVertex3f( 1.0f,-1.0f,-1.0f);
+			glVertex3f(-1.0f,-1.0f,-1.0f);
+			glVertex3f(-1.0f, 1.0f,-1.0f);
+			glVertex3f( 1.0f, 1.0f,-1.0f);
+			// Left
+			glColor3f(0.0f, 0.0f, 1.0f); // Blue
+			glVertex3f(-1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, 1.0f,-1.0f);
+			glVertex3f(-1.0f,-1.0f,-1.0f);
+			glVertex3f(-1.0f,-1.0f, 1.0f);
+			// Right
+			glColor3f(1.0f, 0.0f, 1.0f); // Violet
+			glVertex3f( 1.0f, 1.0f,-1.0f);
+			glVertex3f( 1.0f, 1.0f, 1.0f);
+			glVertex3f( 1.0f,-1.0f, 1.0f);
+			glVertex3f( 1.0f,-1.0f,-1.0f);
+		glEnd();
+	glPopMatrix();
 
 	Enable2D();
-		glBegin(GL_TRIANGLES);
-			glColor3ub(255, 0, 0);
-				glVertex2d(0, 0);
-			glColor3ub(0, 255, 0);
-				glVertex2d(100, 0);
-			glColor3ub(0, 0, 255);
-				glVertex2d(50, 50);
-		glEnd();
-	Disable2D();
-
-	glBegin(GL_QUADS);
-		// TOP
-		glColor3f(0.0f, 1.0f, 0.0f); // Green
-		glVertex3f( 1.0f, 1.0f,-1.0f); // Top Right
-		glVertex3f(-1.0f, 1.0f,-1.0f); // Top Left
-		glVertex3f(-1.0f, 1.0f, 1.0f); // Bottom Left
-		glVertex3f( 1.0f, 1.0f, 1.0f); // Bottom Right
-		// Bottom
-		glColor3f(1.0f, 0.5f, 0.0f); // Orange
-		glVertex3f( 1.0f,-1.0f, 1.0f);
-		glVertex3f(-1.0f,-1.0f, 1.0f);
-		glVertex3f(-1.0f,-1.0f,-1.0f);
-		glVertex3f( 1.0f,-1.0f,-1.0f);
-		// Front
-		glColor3f(1.0f, 0.0f, 0.0f); // Red
-		glVertex3f( 1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f,-1.0f, 1.0f);
-		glVertex3f( 1.0f,-1.0f, 1.0f);
-		// Back
-		glColor3f(1.0f, 1.0f, 0.0f); // Yellow
-		glVertex3f( 1.0f,-1.0f,-1.0f);
-		glVertex3f(-1.0f,-1.0f,-1.0f);
-		glVertex3f(-1.0f, 1.0f,-1.0f);
-		glVertex3f( 1.0f, 1.0f,-1.0f);
-		// Left
-		glColor3f(0.0f, 0.0f, 1.0f); // Blue
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f,-1.0f);
-		glVertex3f(-1.0f,-1.0f,-1.0f);
-		glVertex3f(-1.0f,-1.0f, 1.0f);
-		// Right
-		glColor3f(1.0f, 0.0f, 1.0f); // Violet
-		glVertex3f( 1.0f, 1.0f,-1.0f);
-		glVertex3f( 1.0f, 1.0f, 1.0f);
-		glVertex3f( 1.0f,-1.0f, 1.0f);
-		glVertex3f( 1.0f,-1.0f,-1.0f);
+	glBegin(GL_TRIANGLES);
+	glColor3ub(255, 0, 0);
+	glVertex2d(0, 0);
+	glColor3ub(0, 255, 0);
+	glVertex2d(100, 0);
+	glColor3ub(0, 0, 255);
+	glVertex2d(50, 50);
 	glEnd();
+	Disable2D();
 }
--- OpenGLT1/OpenGLT1/main.cpp
+++ OpenGLT1/OpenGLT1/main.cpp
@@ -1,15 +1,8 @@
-#define WIN32_WIND
-
-#include <Windows.h>
-#include <gl/gl.h>
-#include <gl/glu.h>
-
+#include "glfw.h"
 #include "OpenGLSys.h"
 #include "timer.h"
 
-
-HDC hDC;				// Private GDI Device Context
-bool active=true;		// Window Active Flag
+int active=1;		// Window Active Flag
 bool fullscreen=false;	// Fullscreen Flag
 
 // Ugly stuff
@@ -17,229 +10,26 @@ long wWidth = 800;
 long wHeight = 600;
 long wBits = 32;
 
-OpenGLSys *glRender = NULL;
-CHiResTimer *g_hiResTimer = NULL;
+OpenGLSys *glRender = 0;
+CHiResTimer *g_hiResTimer = 0;
 
-void ShutDown()
+void shutDown()
 {
 	delete glRender;
 	delete g_hiResTimer;
-	if (fullscreen)
-	{
-		ChangeDisplaySettings(NULL, 0);
-		ShowCursor(true);
-	}
-}
-void SetupPixelFormat(HDC hDC)
-{
-	int pixelFormat;
-
-	static PIXELFORMATDESCRIPTOR pfd =
-	{
-		sizeof(PIXELFORMATDESCRIPTOR),
-		1, // Version Number
-		PFD_DRAW_TO_WINDOW | // Supports being in a window
-		PFD_SUPPORT_OPENGL | // Supports OpenGL
-		PFD_DOUBLEBUFFER, // Supports Double Buffering
-		PFD_TYPE_RGBA, // Request RGBA Format
-		32, // Sets the Colour Depth
-		0, 0, 0, 0, 0, 0, // Ignore Colour Bits
-		0, // No Alpha Buffer
-		0, // Shift Bit Ignored
-		0, // No Accumulation Buffer
-		0, 0, 0, 0, // Accumulation Bits Ignored
-		16, // 16bit Depth Buffer
-		0, //  NO Stencil Buffer
-		0, // No Auxiliary Buffer
-		PFD_MAIN_PLANE, // Main Drawing Layer
-		0, // Reserved
-		0, 0, 0 // Layer Masks Ignored
-	};
-
-	pixelFormat = ChoosePixelFormat(hDC, &pfd);
-	SetPixelFormat(hDC, pixelFormat, &pfd);
+	glfwTerminate();
 }
 
-LRESULT CALLBACK MainWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+int main(void)
 {
-	static HDC hDC;
-	static HGLRC hRC;
-	int height, width;
-	switch (uMsg)
-	{
-		case WM_CREATE:
-			// Makes more sense to init OpenGL when the Window creation is called through the API
-			hDC = GetDC(hWnd);
-			SetupPixelFormat(hDC);
-
-			hRC = wglCreateContext(hDC);
-			wglMakeCurrent(hDC, hRC);
-			break;
-
-		case WM_DESTROY:
-		case WM_QUIT:
-		case WM_CLOSE: // X has been pressed
-			{
-				// Deselect the renering context, then delete it
-				wglMakeCurrent(hDC, NULL);
-				wglDeleteContext(hRC);
-				PostQuitMessage(0);
-				break;
-			}
-		case WM_SIZE:
-			{
-				height = HIWORD(lParam);
-				width = LOWORD(lParam);
-				glRender->SetProjection(width, height); // Width -> Height
-				break;
-			}
-		case WM_PAINT:              // Sent when another app or windows wants to draw on this
-			PAINTSTRUCT ps;
-			BeginPaint(hWnd, &ps);
-			EndPaint(hWnd, &ps);
-			break;
-
-		case WM_LBUTTONDOWN:
-			break;
-		case WM_RBUTTONDOWN:
-			break;
-		case WM_MOUSEMOVE:
-			break;
-		case WM_LBUTTONUP:
-			break;
-		case WM_RBUTTONUP:
-			break;
-		case WM_KEYUP:
-			break;
-
-		case WM_KEYDOWN:
-			int fwKeys;
-			LPARAM keyData;
-			fwKeys = (int)wParam;    // virtual-key code
-			keyData = lParam;          // key data
-
-			switch(fwKeys)
-			{
-			case VK_ESCAPE:
-				PostQuitMessage(0);
-				break;
-			default:
-				break;
-			}
-
-			break; // Out of KeyDOWN
-
-		default:
-			break;
-	}
-	// Pass all unhandled messages to DefWindowProc and let Windows deal with them
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	WNDCLASSEX wc; // Window Class
-	HWND hWnd; // Window Handle
-	MSG msg; // Message structure
-	DWORD dwExStyle; // Window Extended Style
-	DWORD dwStyle; // Window Style
-	RECT windowRect;
-
 	glRender = new OpenGLSys;
 	g_hiResTimer = new CHiResTimer;
 
-	windowRect.left=(long)0;                        // Set Left Value To 0
-	windowRect.right=(long)800; // Set Right Value To Requested Width
-	windowRect.top=(long)0;                         // Set Top Value To 0
-	windowRect.bottom=(long)600;   // Set Bottom Value To Requested Height
-
-	// fill out the window class structure
-	wc.cbSize          = sizeof(WNDCLASSEX);
-	wc.style           = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc     = MainWindowProc;
-	wc.cbClsExtra      = 0;
-	wc.cbWndExtra      = 0;
-	wc.hInstance       = hInstance;
-	wc.hIcon           = LoadIcon(NULL, IDI_APPLICATION);  // default icon
-	wc.hCursor         = LoadCursor(NULL, IDC_ARROW);      // default arrow
-	wc.hbrBackground   = NULL;                             // don't need background
-	wc.lpszMenuName    = NULL;                             // no menu
-	wc.lpszClassName   = "POpenGLApp";
-	wc.hIconSm         = LoadIcon(NULL, IDI_WINLOGO);      // windows logo small icon
-
-	if (!RegisterClassEx(&wc))
-	{
-		MessageBox(NULL, "Failed to register the window class", "ERROR", MB_OK | MB_ICONINFORMATION);
-		return false;
-	}
-
-	if (fullscreen)
-	{
-		/*
-		 * Clears room to store the video settings
-		 */
-		DEVMODE dmScreenSettings; // Device Mode
-		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings)); // Make sure the memory has been cleared
-		dmScreenSettings.dmSize=sizeof(dmScreenSettings); // Size of the struct
-		dmScreenSettings.dmPelsHeight = wHeight;
-		dmScreenSettings.dmPelsWidth = wWidth;
-		dmScreenSettings.dmBitsPerPel = wBits; // Bits per pixel
-		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
-
-		// Try to set the selected mode
-		if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
-		{
-			// If it fails, ask to quit or attempt to run in a window
-			if (MessageBox(NULL, "Fullscreen is not supported by your video card\n Use Windows Mode Instead?", "",
-				MB_YESNO | MB_ICONINFORMATION) == IDYES)
-			{
-				fullscreen = false;
-			}
-			else
-			{
-				MessageBox(NULL, "Closing the application", "ERROR", MB_OK | MB_ICONSTOP);
-				return false;
-			}
-		}
-	}
-
-	if (fullscreen)
-	{
-		dwExStyle = WS_EX_APPWINDOW;
-		dwStyle = WS_POPUP;
-		ShowCursor(false);
-	}
-	else
-	{
-		dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
-		dwStyle = WS_OVERLAPPEDWINDOW;
-	}
-
-	AdjustWindowRectEx(&windowRect, dwStyle, false, dwExStyle); // And adjust window to the REQUESTED size
-	if (!(hWnd=CreateWindowEx(  dwExStyle,
-		"POpenGLApp",							// Class Name
-		"Pete's Little App",					// Window Title
-		WS_CLIPSIBLINGS |						// Required Window Style
-		WS_CLIPCHILDREN |						// Required Window Style
-		dwStyle,								// Selected Window Style
-		0, 0,									// Window Position
-		windowRect.right-windowRect.left,		// Calculate Adjusted Window Width
-		windowRect.bottom-windowRect.top,		// Calculate Adjusted Window Height
-		NULL,									// No Parent Window
-		NULL,									// No Menu
-		hInstance,								// Instance
-		NULL)))									// Don't Pass Anything To WM_CREATE
-	{
-		MessageBox(NULL, "Window Creation Error", "ERROR", MB_OK | MB_ICONEXCLAMATION);
-		return 0;
-	}
-	// Create the window
-	hDC = GetDC(hWnd);
-
-	ShowWindow(hWnd, SW_SHOW); // Display
-	UpdateWindow(hWnd); // Show
-
+	glfwInit();
+	glfwOpenWindow(wWidth, wHeight, 5, 6, 5, 8, 24, 0, GLFW_WINDOW);
+	glfwSetWindowTitle("Pete's Little App");
 	glRender->Init();
+	glRender->SetProjection(wWidth, wHeight);
 
 	// Init the timer that will 'limit' the framerate and help the program deal with updates based on time rather than frames elapsed
 	g_hiResTimer->Init();
@@ -248,20 +38,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmd
 	{
 		glRender->Update(g_hiResTimer->GetElapsedSeconds(1));
 		glRender->Render();
-		SwapBuffers(hDC);
-
-		while (PeekMessage (&msg, NULL, 0, 0, PM_NOREMOVE))
-		{
-			if (!GetMessage (&msg, NULL, 0, 0))
-			{
-				active = false;
-				break;
-			}
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
+		glfwSwapBuffers();
+		active = glfwGetWindowParam(GLFW_OPENED); // if the window is closed, active is false, else true.
 	}
 
-	ShutDown();
+	shutDown();
 	return 0; // Won't be reached
 }
